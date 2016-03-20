edf8e86b234342a7330ed4873a54784043a5a6fa
Varun Vachhar <varunvachhar@gmail.com>
2016-03-20 12:22:17 -0400

    wip

--- src/components/sliceable-polygon.js
+++ src/components/sliceable-polygon.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {shape, intersect} from 'svg-intersections';
 import clrs from '../styles/clrs';
 import {aperture} from 'ramda';
 import Markers from './markers';
 import Line from './line';
+import { getIntersections, split } from '../utils/slicer';
 
 const SliceablePolygon = ({ poly, line }) => {
   if (!line.x2 || !line.y2) {
@@ -56,60 +56,3 @@ SliceablePolygon.propTypes = {
 };
 
 export default SliceablePolygon;
-
-
-/**
- * Utils
- */
-function getIntersections(poly, line) {
-  return intersect(
-    shape('polygon', { points: poly.join(' ') }),
-    shape('line', line)
-  ).points;
-}
-
-function containsIntersection(side, i) {
-  const a = side[0];
-  const b = side[1];
-
-  const xValid = (a[0] <= b[0]) ? i.x >= a[0] && i.x <= b[0] :
-                                  i.x >= b[0] && i.x <= a[0];
-  const yValid = (a[1] <= b[1]) ? i.y >= a[1] && i.y <= b[1] :
-                                  i.y >= b[1] && i.y <= a[1];
-
-  return xValid && yValid;
-}
-
-function hasIntersection(side, intersections) {
-  return intersections.reduce((res, i) => {
-    return containsIntersection(side, i) ? [i.x, i.y] : res;
-  }, null);
-}
-
-function split(sides, intersections) {
-  let chunk1Active = false;
-
-  return sides.reduce((poly, side) => {
-    const ints = hasIntersection(side, intersections);
-
-    if (!chunk1Active) {
-      if (ints) {
-        poly.chunk1.push(...[side[0], ints]);
-        poly.chunk2.push(...[ints, side[1]]);
-        chunk1Active = true;
-      } else {
-        poly.chunk1.push(...side);
-      }
-    } else {
-      if (ints) {
-        poly.chunk2.push(...[side[0], ints]);
-        poly.chunk1.push(...[ints, side[1]]);
-        chunk1Active = false;
-      } else {
-        poly.chunk2.push(...side);
-      }
-    }
-
-    return poly;
-  }, { chunk1: [], chunk2: [] });
-}
---
+++ src/utils/slicer.js
@@ -0,0 +1,57 @@
+import {shape, intersect} from 'svg-intersections';
+
+/**
+ * Slicer
+ */
+export function getIntersections(poly, line) {
+  return intersect(
+    shape('polygon', { points: poly.join(' ') }),
+    shape('line', line)
+  ).points;
+}
+
+export function containsIntersection(side, i) {
+  const a = side[0];
+  const b = side[1];
+
+  const xValid = (a[0] <= b[0]) ? i.x >= a[0] && i.x <= b[0] :
+                                  i.x >= b[0] && i.x <= a[0];
+  const yValid = (a[1] <= b[1]) ? i.y >= a[1] && i.y <= b[1] :
+                                  i.y >= b[1] && i.y <= a[1];
+
+  return xValid && yValid;
+}
+
+export function hasIntersection(side, intersections) {
+  return intersections.reduce((res, i) => {
+    return containsIntersection(side, i) ? [i.x, i.y] : res;
+  }, null);
+}
+
+export function split(sides, intersections) {
+  let chunk1Active = false;
+
+  return sides.reduce((poly, side) => {
+    const ints = hasIntersection(side, intersections);
+
+    if (!chunk1Active) {
+      if (ints) {
+        poly.chunk1.push(...[side[0], ints]);
+        poly.chunk2.push(...[ints, side[1]]);
+        chunk1Active = true;
+      } else {
+        poly.chunk1.push(...side);
+      }
+    } else {
+      if (ints) {
+        poly.chunk2.push(...[side[0], ints]);
+        poly.chunk1.push(...[ints, side[1]]);
+        chunk1Active = false;
+      } else {
+        poly.chunk2.push(...side);
+      }
+    }
+
+    return poly;
+  }, { chunk1: [], chunk2: [] });
+}
